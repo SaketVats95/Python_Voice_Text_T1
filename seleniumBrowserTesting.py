84b64a7c65427883c2ef3bd3f7f6f502ec638ed0
Saket Vats <saketvatslpu@gmail.com>
2019-05-08 17:36:41 +0530

    Worked on selenium file at work place

--- seleniumBrowserTesting.py
+++ seleniumBrowserTesting.py
@@ -3,18 +3,22 @@ from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
 binary = FirefoxBinary(r'C:\Program Files\Mozilla Firefox\firefox.exe')
 driver = webdriver.Firefox(
-    firefox_binary=binary, executable_path=r'C:\Users\Saket\Downloads\geckodriver-v0.24.0-win64\geckodriver.exe')
+    firefox_binary=binary)
 # driver.get("http://econpy.pythonanywhere.com/ex/001.html")
-#searchString = input("Enter the search query")
-#driver.get("http://viewpure.com/search?q="+"old bollywood songs")
+searchString = input("Enter the search query")
+driver.get("http://viewpure.com/search?q="+searchString)
 # driver.implicitly_wait(40)
 
-#linksInfo = driver.find_elements_by_xpath('//a[@class="live-link2"]')
-# for i in range(len(buyers)):
-# for link in linksInfo:
-#   links.append(link.get_attribute('href'))
-# print(links)
-driver.get('http://viewpure.com/PGaREkyfKPs?ref=search')  # links[0])
+linksInfo = driver.find_elements_by_xpath('//a[@class="live-link2"]')
+#for i in range(len(buyers)):
+links=[]
+for link in linksInfo:
+    links.append(link.get_attribute('href'))
+
+for i in range(len(links)):
+    print(i+1,":",linksInfo[i].text)
+videoitem=int(input())
+driver.get(links[i-1])  # links[0])
 playlink = driver.find_element_by_xpath('//div[@class="video"]')
 print(playlink.text)
 playlink.click()
