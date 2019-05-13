001c49fb2cef29e02e92bf0f235e0a7abf94165e
Saket Vats <saketvatslpu@gmail.com>
2019-05-13 18:07:22 +0530

    Working on Wiki links

--- testYoutube.py
+++ testYoutube.py
@@ -1,20 +1,20 @@
-import urllib.request
+import urllib
 from bs4 import BeautifulSoup
 import webbrowser
 
 
 def OpenYotube(data):
     textToSearch = 'Hello World'
-    query = urllib.parse.quote(textToSearch)
-    url = "https://www.youtube.com/results?search_query=" + query
-    response = urllib.request.urlopen(url)
+    #query = urllib.parse.quote(textToSearch)
+    url = "https://www.youtube.com/results?search_query=" + textToSearch
+    response = urllib.urlopen(url)
     html = response.read()
 
     soup = BeautifulSoup(html, 'html.parser')
-    f= open("guru99.txt","w+")
-    f.write(soup.prettify)
+    #f= open("guru99.txt","w+")
+    #f.write(soup.prettify)
     for vid in soup.findAll(attrs={'class': 'yt-uix-tile-link'}):
-        print('https://www.youtube.com' + vid['href']+vid['text'])
+        print('https://www.youtube.com' + vid['href'])
 
 OpenYotube('Hello')
 
