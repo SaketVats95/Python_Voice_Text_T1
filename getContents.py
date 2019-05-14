1a68d1dffb7a01a19298f155ea06f7517c780d13
Saket Vats <saketvatslpu@gmail.com>
2019-05-14 17:50:36 +0530

    Extract information from Google Contacts

--- getContents.py
+++ getContents.py
@@ -1,14 +1,37 @@
 import urllib.request
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
-f=open("ne.txt","r")
+
+url="https://contacts.google.com/"
+driver=webdriver.Firefox()
+driver.get(url)
+
+f=open("userinfo.txt","r+")
+username=f.readline()
+#f.write(driver.page_source)
+userNameInput=driver.find_element_by_xpath("//input[@name='identifier']")
+print(userNameInput.get_attribute('type'))
+userNameInput.send_keys(username)
+button=driver.find_element_by_xpath("//*[text()='Next']")
+#driver.get(url)
+print(button.text)
+button.click()
+driver.get(url)
+contactName=driver.find_element_by_xpath("//div[@class='E6Tb7b']")
+for i in contactName:
+    print(i.text)
+
+f.close()
+
+
+#f=open("ne.txt","r")
 #print(f.read())
-soup=BeautifulSoup(f.read(), 'html.parser')
-strhtm = soup.prettify()
+#soup=BeautifulSoup(f.read(), 'html.parser')
+#strhtm = soup.prettify()
+
 
 # Print the first few characters
-print (strhtm)
-f.close()
-f=open("newFormated.html","w+")
-f.write(strhtm)
-f.close()
+#print (strhtm)
+#f.close()
+
